1e7eec583e24ffea8c81c23d0b9dcdf9886976bd
Bambang Hermawan <blackblizt@gmail.com>
2017-12-09 14:04:09 +0700

    404... brain not found!

--- application/views/dosen/dashboard.php
+++ application/views/dosen/dashboard.php
@@ -7,93 +7,40 @@ $this->load->view('dosen/template/topbar');
 $this->load->view('dosen/template/sidebar');
 ?>
 
-<!-- Content Header (Page header) -->
 <section class="content-header">
     <h1>
-        Dashboard
+        Dosen Dashboard
         <small>Control panel</small>
     </h1>
     <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Dashboard Dosen</li>
+        <li class="active">Dosen Dashboard</li>
     </ol>
 </section>
 
-<!-- Main content -->
 <section class="content">
-    <!-- Small boxes (Stat box) -->
     <div class="row">
-        <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-aqua">
-                <div class="inner">
-                    <h3>150</h3>
-                    <p>New Orders</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-bag"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-        </div><!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-green">
-                <div class="inner">
-                    <h3>53<sup style="font-size: 20px">%</sup></h3>
-                    <p>Bounce Rate</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-stats-bars"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-        </div><!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-yellow">
-                <div class="inner">
-                    <h3>44</h3>
-                    <p>User Registrations</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-person-add"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-        </div><!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-red">
-                <div class="inner">
-                    <h3>65</h3>
-                    <p>Unique Visitors</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-pie-graph"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-        </div><!-- ./col -->
-    </div><!-- /.row -->
-    <!-- Main row -->
-    <div class="row">
-        <!-- Left col -->
-        <section class="col-lg-7 connectedSortable">
-            <!-- Custom tabs (Charts with tabs)-->
-            <div class="nav-tabs-custom">
-                <!-- Tabs within a box -->
-                </div>
-            </div><!-- /.nav-tabs-custom -->
+        <div class="col-md-12">
+            <div class="box box-info box-header with-border">
 
-            <!-- Chat box -->
-                 </div>
             </div>
-            
-        </section><!-- right col -->
-    </div><!-- /.row (main row) -->
-
-</section><!-- /.content -->
+            <?php if(isset($msg) || validation_errors() !== ''): ?>
+                <div class="alert alert-warning alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <h4><i class="icon fa fa-warning"></i> Peringatan!</h4>
+                    <?= validation_errors();?>
+                    <?= isset($msg)? $msg: ''; ?>
+                </div>
+            <?php endif; ?>
+            <?php if ($this->session->flashdata('msg')) { ?>
+                <div class="alert alert-success alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <h4><i class="icon fa fa-warning"></i> Peringatan!</h4>
+                    <?php echo $this->session->flashdata('msg');?>
+                </div>
+            <?php } ?>
+    </div>
+</section>
 
 
 <?php
